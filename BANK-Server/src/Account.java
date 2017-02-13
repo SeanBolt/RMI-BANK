ae6ffe586875bd90733132331144b57a92435454
Sean Bolton <sean@seanbolton.ie>
2017-02-13 15:25:28 +0000

    Built out Account Class

--- BANK-Server/src/Account.java
+++ BANK-Server/src/Account.java
@@ -1,3 +1,27 @@
 public class Account {
+	private String accountName;
+	private int accountNumber = ++lastAssignedNumber;
+	private static int lastAssignedNumber;
+	
+	public Account(String accountName) {
+		this.accountName = accountName;
+	}
 
+	//	Getters & Setters
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}	
+	
 }
